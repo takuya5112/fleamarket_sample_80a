a4d305c656ed0ca4f1d95e01c2edb37ce57983b7
shun-kubo8730 <bbpks080730@gmail.com>
2020-08-06 19:23:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resources :items do
+    resources :comments
+  end
+  resources :users
+  resources :user_addresses
+  resources :user_profiles
+  resources :credit_cards
 end
