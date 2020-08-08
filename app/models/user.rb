317ee19a7d6b7afe2f0db69387b3e35486f5926b
sasaki5963 <masato11119@gmail.com>
2020-08-09 00:10:09 +0900

    ログイン、新規登録画面ビュー完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, uniqueness: true
+
   has_many :seller_items, foreign_key: "seller_id", class_name: "Item", dependent: :destroy
   has_many :buyer_items, foreign_key: "buyer_id", class_name: "Item", dependent: :nullify
   has_many :comments, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_115619) do
+ActiveRecord::Schema.define(version: 2020_08_07_170248) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
