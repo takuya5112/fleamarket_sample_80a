3207c01409819c273ee6aef3dd23aa7a7e8d67cf
shun-kubo8730 <bbpks080730@gmail.com>
2020-08-14 19:05:22 +0900

    active hash data creation

--- app/models/postage_days.rb
+++ app/models/postage_days.rb
@@ -3,11 +3,9 @@ class PostageDays < ActiveHash::Base
 
   has_many :items
 
-  # 以下は例のつもりです。 wataya
   self.data = [
-      {id: 1, day: '2'},
-      {id: 2, day: '5'}, 
-      {id: 3, day: '7'}
+      {id: 1, day: '1日〜2日で発送'},
+      {id: 2, day: '2日〜3日で発送'}, 
+      {id: 3, day: '4日〜7日で発送'},
   ]
-
 end
