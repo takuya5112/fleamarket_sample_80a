8efc8132f4f257cbdac70a0a89d33832b69e53d7
shun-kubo8730 <bbpks080730@gmail.com>
2020-08-14 10:07:55 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
 
+  before_action :set_item, only: [:show, :destroy]
+
   def index
     @items = Item.all.order('id DESC').limit(10)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
   
   def new
@@ -17,9 +18,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @items = Item.find(params[:id])
-    @items.destroy
+    @item.destroy
     redirect_to  delete_done_items_path
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
